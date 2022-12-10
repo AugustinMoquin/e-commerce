9811b06baf4e5744d460825b579c80cb93a4691d
Augustin Moquin <augustin.moquin@ynov.com>
2022-12-10 14:22:35 +0100

    add image

--- php/ajout_produit/ajout_produit.php
+++ php/ajout_produit/ajout_produit.php
@@ -8,6 +8,52 @@
 </head>
 <body>
 
+<?php
+include ("../db_connection.php");
+
+$conn = openCon();
+    function GetImageExtension($imagetype)
+     {
+       if (empty($imagetype)) return false;
+       switch ($imagetype){
+           case 'image/bmp': return '.bmp';
+           case 'image/gif': return '.gif';
+           case 'image/jpeg': return '.jpg';
+           case 'image/png': return '.png';
+           default: return false;
+       }
+     }
+
+
+
+if (!empty($_FILES["uploadedimage"])) {
+
+    $file_name=$_FILES["uploadedimage"]["name"];
+    $temp_name=$_FILES["uploadedimage"]["tmp_name"];
+    $imgtype=$_FILES["uploadedimage"]["type"];
+    $ext= GetImageExtension($imgtype);
+    $imagename= $file_name."-".date("d-m-Y")."-".time().$ext;
+    $target_path = "../images/".$imagename;
+
+    if (move_uploaded_file($temp_name, $target_path)) {
+        $query_upload="INSERT INTO products (images_path) VALUES ('$target_path')";
+        if ($conn->query($query_upload) === true) {
+            echo "New record created successfully";
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+          }
+    }else {
+
+    exit("Error While uploading image on the server");
+    }
+}
+closeCon($conn);
+echo $target_path;
+?>
+
+
+
+
 <header>
 
 </header>
@@ -18,17 +64,23 @@
 
 <div class="container">
     <div class="container_insertImage">
-        <h3>Image du produit</h3>
-
-        <form enctype="multipart/form-data" action="process.php" method="post">
+        <form enctype="multipart/form-data" action="" method="post">
             <label>Upload Your Image
                 <input type="file" name="uploadedimage" />
             </label>
-
-            <input type="submit" value="Submit Page" />
+            <input type="submit" value="Envoyer l'image" />
         </form>
-
+        <h3>Image du produit</h3>
+        <div class="container_insertImage_Image">
+            <img src="<?php echo $target_path; ?>" alt="Image">
+        </div>
     </div>
+    <form action="process.php">
+
+
+
+    <input type="submit" value="Submit Page" />
+    </form>
 </div>
 
 
@@ -37,6 +89,9 @@
 <footer>
 
 </footer>
+
+
+
 
 
 </body>
