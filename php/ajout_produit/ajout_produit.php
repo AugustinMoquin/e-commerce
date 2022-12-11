233e7697838016d12f637a0779ee7d0b8ece561d
augustin moquin <augustinmqn@gmail.com>
2022-12-11 19:24:09 +0100

    style pade ajout de produit

--- php/ajout_produit/ajout_produit.php
+++ php/ajout_produit/ajout_produit.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/add_product.php">
+    <link rel="stylesheet" href="../ajout_produit/assets/css/add_product.css">
     <title>Document</title>
 </head>
 <body>
@@ -39,7 +39,7 @@ if (!empty($_FILES["uploadedimage"])) {
     if (move_uploaded_file($temp_name, $target_path)) {
         $query_upload="INSERT INTO products (images_path) VALUES ('$target_path')";
         if ($conn->query($query_upload) === true) {
-            echo "New record created successfully";
+            //echo "New record created successfully";
           } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
           }
@@ -49,48 +49,98 @@ if (!empty($_FILES["uploadedimage"])) {
     }
 }
 closeCon($conn);
-echo $target_path;
+//echo $target_path;
 ?>
 
 
 
 
-<header>
-
-</header>
-
-<div class="Title">
-    <h1>AJOUT DE PRODUIT</h1>
-</div>
-
 <div class="container">
-    <div class="container_insertImage">
-        <form enctype="multipart/form-data" action="" method="post">
-            <label>Upload Your Image
-                <input type="file" name="uploadedimage" />
-            </label>
-            <input type="submit" value="Envoyer l'image" />
-        </form>
-        <h3>Image du produit</h3>
-        <div class="container_insertImage_Image">
-            <img src="<?php echo $target_path; ?>" alt="Image">
+    <header>
+
+    </header>
+
+    <div class="Title">
+        <h1>AJOUT DE PRODUIT</h1>
+    </div>
+    <div class="container_all">
+        <div class="container_insertImage">
+            <form enctype="multipart/form-data" action="" method="post">
+                <label>Upload Your Image
+                    <input type="file" name="uploadedimage" />
+                </label>
+                <input type="submit" value="Envoyer l'image" />
+            </form>
+            <h3>Image du produit</h3>
+            <div class="image_container">
+                <img src="<?php echo $target_path; ?>" class = "container_insertImage_Image" alt="Image">
+            </div>
+        </div>
+        <div class="container_info">
+            <form action="process.php" class="container_info">
+                <div>
+                    <label class="Name"> Nom du produit
+                        <input type="text" name = "name" />
+                    </label>
+                </div>
+
+                <div class="price_contain">
+                    <label class="price"> Prix
+                        <input type="text" name = "price" class = "price_input"  />
+                    </label>
+                    <label class="price">Pourcentage de réduction
+                        <input type="text" name = "reduc" class = "reduc_input"/>
+                    </label>
+                </div>
+
+                <label class="category">Catégories
+                    <select class = "category_select" name = "category" >
+                        <option>jsp</option>
+                    </select>
+                </label>
+
+                <div class="container_status">
+                    <div class="status">
+                        <label>Status du produit
+                            <select class = "status" name = "status" >
+                                <option>en stock</option>
+                                <option>commandé</option>
+                                <option>rupture</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    <div >
+                        <label class="quantity">Quantité
+                            <input type="number" class = "quantity_input"  name = "quantity" />
+                        </label>
+                    </div>
+
+                    <div >
+                        <label class="taxe">Taxe
+                            <label>oui
+                                <input type="radio" name = "taxe" value = "oui" />
+                            </label>
+                            <label>non
+                                <input type="radio" name = "taxe" value = "non" />
+                            </label>
+                        </label>
+                    </div>
+                </div>
+
+                <input type="submit" value="Submit Page" />
+            </form>
         </div>
     </div>
-    <form action="process.php">
+    
+    
+    
+    
+    <footer>
         
-
-
-        <input type="submit" value="Submit Page" />
-    </form>
+    </footer>
+        
 </div>
-
-
-
-
-<footer>
-
-</footer>
-
 
 
 
