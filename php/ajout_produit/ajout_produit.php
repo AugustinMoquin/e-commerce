65d058f171601c3409235cc7fd78f5ad1ba79904
augustin moquin <augustinmqn@gmail.com>
2022-12-30 12:43:51 +0100

    page produit

--- php/ajout_produit/ajout_produit.php
+++ php/ajout_produit/ajout_produit.php
@@ -26,7 +26,7 @@
     }
 
     if (isset($_POST['name']) && $_POST['price'] && $_POST['reduc'] && $_POST['category'] 
-    && $_POST['status'] && $_POST['quantity'] && $_POST['taxe'] && $_POST['description'] >0 ){
+    && $_POST['status'] && $_POST['quantity'] && $_POST['taxe'] && $_POST['description'] >0 && !empty($_FILES["uploadedimage"]) ){
         
         $name = $_POST['name'];
         $price = $_POST['price'];
@@ -36,11 +36,7 @@
         $quantity = $_POST['quantity'];
         $taxe = $_POST['taxe'];
         $description = $_POST['description'];
-    }
 
-
-
-    if (!empty($_FILES["uploadedimage"])) {
 
         $file_name=$_FILES["uploadedimage"]["name"];
         $temp_name=$_FILES["uploadedimage"]["tmp_name"];
@@ -55,18 +51,48 @@
             VALUES ('$target_path', '$name', '$price', '$reduc', '$category',
              '$status', '$quantity', '$taxe', '$description')";
             if ($conn->query($query_upload) === true) {
-                //echo "New record created successfully";
+                echo "New record created successfully";
             } else {
                 echo "Error: " . $sql . "<br>" . $conn->error;
             }
         }else {
-            exit("Error While uploading image on the server");
+            echo "Error While uploading image on the server";
+            sleep(3);
+            header('Location: http://localhost/e-commerce/php/ajout_produit/ajout_produit.php');
         }
+
     }
 
 
+
+    // if (!empty($_FILES["uploadedimage"])) {
+
+    //     $file_name=$_FILES["uploadedimage"]["name"];
+    //     $temp_name=$_FILES["uploadedimage"]["tmp_name"];
+    //     $imgtype=$_FILES["uploadedimage"]["type"];
+    //     $ext= GetImageExtension($imgtype);
+    //     $imagename= $file_name."-".date("d-m-Y")."-".time().$ext;
+    //     $target_path = "../images/".$imagename;
+
+    //     if (move_uploaded_file($temp_name, $target_path)) {
+    //         $query_upload="INSERT INTO products (images_path, NAME, regular_price, discount_price,
+    //          category, product_status, quantity, taxable, description)
+    //         VALUES ('$target_path', '$name', '$price', '$reduc', '$category',
+    //          '$status', '$quantity', '$taxe', '$description')";
+    //         if ($conn->query($query_upload) === true) {
+    //             //echo "New record created successfully";
+    //         } else {
+    //             echo "Error: " . $sql . "<br>" . $conn->error;
+    //         }
+    //     }else {
+    //         header('Location: http://localhost/e-commerce/php/ajout_produit/ajout_produit.php');
+    //         echo "Error While uploading image on the server";
+
+    //     }
+    // }
+
+
     closeCon($conn);
-    //echo $target_path;
 ?>
 
 
@@ -82,12 +108,6 @@
     </div>
     <div class="container_all">
         <form enctype="multipart/form-data" action="" method="post">
-            <div class="container_insertImage">
-                    <!-- <label>Upload Your Image
-                        <input type="file" name="uploadedimage" />
-                    </label> -->
-            </div>
-
             <div class="container_info">
                 <label>Upload Your Image
                     <input type="file" name="uploadedimage" />
@@ -156,91 +176,7 @@
             </div>  
         </form>
     </div>    
-</div>
-
-
-        <!-- <div class="container_insertImage">
-             <form enctype="multipart/form-data" action="" method="post">
-                <label>Upload Your Image
-                    <input type="file" name="uploadedimage" />
-                </label>
-                <input type="submit" value="Envoyer l'image" />
-            </form>
-            <h3>Image du produit</h3>
-            <div class="image_container">
-                <img src="<?php echo $target_path; ?>" class = "container_insertImage_Image" alt="Image">
-            </div>
-        </div>
-        <div class="container_info">
-            <form action="" class="container_info" method="POST">
-                <div>
-                    <label class="name"> Nom du produit
-                        <input type="text" name = "name" class = "name_input"/>
-                    </label>
-                </div>
-
-                <div class="price_contain">
-                    <label class="price"> Prix
-                        <input type="text" name = "price" class = "price_input"  />
-                    </label>
-                    <label class="price">Pourcentage de réduction
-                        <input type="text" name = "reduc" class = "reduc_input"/>
-                    </label>
-                </div>
-
-                <label class="category">Catégories
-                    <select class = "category_select" name = "category" >
-                        <option>jsp</option>
-                    </select>
-                </label>
-
-                <div class="container_status">
-                    <div class="status">
-                        <label>Status du produit
-                            <select class = "status" name = "status" >
-                                <option>en stock</option>
-                                <option>commandé</option>
-                                <option>rupture</option>
-                            </select>
-                        </label>
-                    </div>
-
-                    <div >
-                        <label class="quantity">Quantité
-                            <input type="number" class = "quantity_input"  name = "quantity" />
-                        </label>
-                    </div>
-
-                    <div >
-                        <label class="taxe">Taxe
-                            <label>oui
-                                <input type="radio" name = "taxe" value = "oui" />
-                            </label>
-                            <label>non
-                                <input type="radio" name = "taxe" value = "non" />
-                            </label>
-                        </label>
-                    </div>
-
-                </div>
-                <div>
-                    <label class = "description"> Description
-                        <input type="text" class = "description_input" name = "description">
-                    </label>
-                </div>
-                <div name="image_path" value = "<?php $target_path ?>" ></div>
-
-                <div class="wrap">
-                    <input type="submit" value="Submit Page" class = " button"/>
-                </div>
-                
-            </form>
-        </div>
-    </div> -->
-    
-    
-    
-    
+</div>    
 <footer>
     
 </footer>
