456bf9396de469e58dcb270350e46d4ffaa71086
augustin moquin <augustinmqn@gmail.com>
2022-12-30 13:05:55 +0100

    back end page d'accueil

--- php/accueil/accueil.php
+++ php/accueil/accueil.php
@@ -11,25 +11,29 @@ echo "Connected Successfully";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../accueil/assets/css/accueil_style.css">
     <title>accueil</title>
 </head>
 <body>
-<div class="products">
+<div class="products" >
              <?php
 
-                $conn = openCon();
-                $sql = "SELECT NAME , regular_price , discount_price , images_path FROM products";
+                $sql = "SELECT NAME , regular_price , discount_price , images_path, Id FROM products";
                 $result = $conn->query($sql);
                 
                 if ($result->num_rows > 0) {
                   // output data of each row
                   while ($row = $result->fetch_assoc()) {
-                    echo  "<div class='ind_user'>  
+                    echo  "<a class='ind_products' 
+                    href='http://localhost/e-commerce/php/product_page/produit.php?&id=". $row["Id"]."'>
+                    <div class='ind_products' name='". $row["Id"]."'>  
+                    <img class='img' src='". $row["images_path"]."'>
                     <div class='info'> Nom: " . $row["NAME"]. "<br> </div>
                     <div class='info'> Price: " . $row["regular_price"]. " <br> </div>
                     <div class='info'> discount: " . $row["discount_price"]. "<br>  </div>
-                    <img class='img' src='". $row["images_path"]."'>
-                    </div>" ;
+                    </div> 
+                    </a>" ;
+                    
                   }
                 } else {
                   echo "0 results";
