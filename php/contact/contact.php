83f4517e8f86c7acac15e181a817af53579b5392
meliha <meliha.ulu@ynov.com>
2022-12-30 16:08:49 +0100

    contact page

--- php/contact/contact.php
+++ php/contact/contact.php
@@ -10,33 +10,33 @@
 </head>
 
 <body>
-    <main>
-        <div class="title">Contact us</div>
-        <div class="title-info">We'll get back to you soon!</div>
+<main>
+        <div class="title"> &#128056; Contact us &#128056;</div>
+        <div class="title-info"> &#128056; We'll get back to you soon! &#128056;</div>
 
         <form action="" method="" class="form">
             <div class="input-group">
                 <input type="text" name="first_name" id="first-name" placeholder="First name">
-                <label for="first-name">First name</label>
+                <label for="first-name"> &#128056; First name</label>
             </div>
 
             <div class="input-group">
                 <input type="text" name="last_name" id="last-name" placeholder="Last Name">
-                <label for="last-name">Last name</label>
+                <label for="last-name"> &#128056; Last name</label>
             </div>
 
             <div class="input-group">
                 <input type="email" name="e-mail" id="e-mail" placeholder="e-mail">
-                <label for="e-mail">e-mail</label>
+                <label for="e-mail"> &#128056; e-mail</label>
             </div>
 
             <div class="textarea-group">
                 <textarea name="message" id="message" rows="5" placeholder="Message"></textarea>
-                <label for="message">Message</label>
+                <label for="message"> &#128056; Message</label>
             </div>
 
             <div class="button-div">
-                <button type="submit">Send</button>
+                <button type="submit"> &#128056; Send &#128056;</button>
             </div>
         </form>
 </body>
